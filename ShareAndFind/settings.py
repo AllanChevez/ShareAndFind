9c4bedf794277305c383fc226d847a5ba2ecf3b5
Kevin Manzanarez <Kevin Manzanarez>
2017-03-22 02:49:21 -0600

    Creacion de archivos estaticos y Global Templates

--- ShareAndFind/settings.py
+++ ShareAndFind/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ShareAndFind.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'global_templates')),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR + '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'global_static'),
+    os.path.join(BASE_DIR, 'media'),
+    )
+
+LOGIN_URL = '/'
