0a17e5436506324c2800fa3ced184a265242d893
Allan Chevez <allanalexandercg@hotmail.com>
2017-03-22 02:10:29 -0600

    Creacion del modelo

---
+++ saf/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Sexo, MyPerfil, Rol, Contacto, NotificacionA, Categoria, Documento, Publicacion, Comentario, NotificacionC,NotificacionD 
+
+admin.site.register(Sexo)
+admin.site.register(MyPerfil)
+admin.site.register(Rol)
+admin.site.register(Contacto)
+admin.site.register(NotificacionA)
+admin.site.register(Categoria)
+admin.site.register(Documento)
+admin.site.register(Publicacion)
+admin.site.register(Comentario)
+admin.site.register(NotificacionC)
+admin.site.register(NotificacionD)
---
+++ saf/models.py
@@ -0,0 +1,108 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+
+@python_2_unicode_compatible
+class Sexo(models.Model):
+	nombre = models.CharField(max_length=25)
+
+	def __str__(self):
+		return self.nombre
+
+@python_2_unicode_compatible
+class MyPerfil(models.Model):
+	nombreUsuario = models.ForeignKey(User)
+	nombre = models.CharField(max_length=25)
+	apellido = models.CharField(max_length=25) 
+	sexo = models.ForeignKey(Sexo)
+	correo = models.EmailField()
+	fechaNacimiento = models.DateField()
+	Avatar = models.ImageField(upload_to = "media/Avatar", null=True, blank =True)
+
+	def __str__(self):
+		return self.nombreUsuario.username
+
+@python_2_unicode_compatible
+class Rol(models.Model):
+	codigo = models.IntegerField()
+	nombre = models.CharField(max_length=25)
+
+	def __str__(self):
+		return self.nombre
+
+@python_2_unicode_compatible
+class Contacto(models.Model):
+	nombreContacto = models.CharField(max_length=25)
+	nombreUsuario = models.ForeignKey(MyPerfil)
+	rol = models.ForeignKey(Rol)
+	fechaAmistad = models.DateField()
+
+	def __str__(self):
+		return self.nombreContacto
+
+@python_2_unicode_compatible
+class NotificacionA(models.Model):
+	nombreContacto = models.ForeignKey(Contacto)
+	nombreUsuario = models.CharField(max_length=25)
+	Decicion = models.IntegerField()
+
+	def __str__(self):
+		return self.nombreContacto.nombreContacto
+
+@python_2_unicode_compatible
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=25)
+
+	def __str__(self):
+		return self.nombre
+
+@python_2_unicode_compatible
+class Documento(models.Model):
+	nombre = models.CharField(max_length=25)
+	archivo = models.FileField(upload_to = "media/archivo")
+	categoria = models.ForeignKey(Categoria)
+	nombreUsuario = models.ForeignKey(MyPerfil)
+
+	def __str__(self):
+		return self.nombre
+
+@python_2_unicode_compatible
+class NotificacionD(models.Model):
+	NombreDocumento = models.ForeignKey(Documento)
+	usuarioDescarga = models.CharField(max_length=25)
+
+	def __str__(self):
+		return str(self.NombreDocumento)
+
+
+@python_2_unicode_compatible
+class Publicacion(models.Model):
+	nombreUsuario = models.ForeignKey(MyPerfil)
+	descripcion = models.TextField()
+	fecha_Publicacion = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.nombreUsuario.nombreUsuario.username
+
+
+@python_2_unicode_compatible
+class Comentario(models.Model):
+	publicacion = models.ForeignKey(Publicacion)
+	contenido = models.TextField(max_length=141)
+	usuario = models.CharField(max_length=25)
+	fecha_Publicacion = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.contenido
+
+
+@python_2_unicode_compatible
+class NotificacionC(models.Model):
+	usuarioComentado = models.ForeignKey(Comentario)
+	usuarioComenta = models.CharField(max_length=25)
+
+	def __str__(self):
+		return self.usuarioComentado.contenido
+
