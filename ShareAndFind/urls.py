6eec4c9c707415d5f62d0bd24b6fb86782cfb673
luisvillaloboz <luisvillaloboz@hotmail.es>
2017-03-26 09:52:49 -0600

    Formulario de login y registro

--- ShareAndFind/urls.py
+++ ShareAndFind/urls.py
@@ -17,12 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
+from seguridad.views import index
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='form_login'),
     url(r'^saf/', include('saf.urls')),
+    url(r'^seguridad/', include('seguridad.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- seguridad/views.py
+++ seguridad/views.py
@@ -1,3 +1,74 @@
+#coding: utf8
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import MyAuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+def index(request):
+	if request.user.is_authenticated():
+		return HttpResponseRedirect('/saf/')
+	form = MyAuthenticationForm()
+	return render(request, 'form_login.html', {'form': form})
 
-# Create your views here.
+def log_out(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+def log_in(request):
+	if request.method == 'POST':
+		form = MyAuthenticationForm(data=request.POST)
+		
+		if form.is_valid():
+			user = authenticate(username=request.POST.get('username'),password=request.POST.get('password'))
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return HttpResponseRedirect('/saf/')
+				else:
+					return HttpResponse('El usuario ha sido desactivado')
+			else:
+				return HttpResponse('Usuario y contraseña invalidos')
+		else:
+			return render(request, 'form_login.html', {'form': form})
+	else:
+		return HttpResponse('No seas travieso')
+
+
+def registro(request):
+    if request.method == 'POST':
+        form = MyAuthenticationForm(request.POST, request.FILES)
+        #if form.is_valid():
+        contrasena = request.POST.get('password')
+        usuario = request.POST.get('username')
+        nombres = request.POST.get('first_name')
+        apellidos = request.POST.get('last_name')
+        email = request.POST.get('email')
+        sexo = request.POST.get('sex')
+        dia = request.POST.get('day')
+        mes = request.POST.get('month')
+        anio = request.POST.get('year')
+        is_staff = False 
+        is_active = True 
+        is_superuser = False
+        usuario = User.objects.create(
+            password = contrasena,
+            is_superuser = is_superuser,
+            username = usuario,
+            is_staff =is_staff,
+            is_active = is_active
+        )
+        usuario1 = User.objects.filter(username=usuario).get()
+        usuario2 = usuario1.username
+        try:
+            usr = MyPerfil(nombreUsuario = 9, nombre = nombres, apellido = apellidos, correo = email, sexo = sexo, fechaNacimiento = (dia,mes,anio))
+            usr.save()
+            return HttpResponseRedirect('/saf/')
+        except Exception as e:
+            return HttpResponse(e)
+
+    else:
+        form = MyAuthenticationForm()
+    context = {'form': form}
+    return render(request, 'index.html', context)
